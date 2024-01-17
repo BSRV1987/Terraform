a1aae4bd3ce4420b5c40b4b4c882e1692633612b
Bala Varanasi <bsrv1987@gmail.com>
2024-01-17 13:54:16 +0100

    keys hidden

--- 001_Aws_lambda/main.tf
+++ 001_Aws_lambda/main.tf
@@ -14,8 +14,8 @@ terraform {
 }
 
 provider "aws" {
-    access_key="AKIA5SMXRGQEHUWMNZHL"
-    secret_key="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"
+    access_key=""
+    secret_key=""
     region="us-east-1"
 
 }
